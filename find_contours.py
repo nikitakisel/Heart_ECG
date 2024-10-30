afeb75436b233f9675f1f85b6c72f795f3f91dc0
Никита Киселев <nikitakiselev@MacBook-Air-Nikita.local>
2024-10-31 00:00:32 +0300

    alternative find of minimums

---
+++ find_contours.py
@@ -0,0 +1,71 @@
+import cv2
+import numpy as np
+import math
+
+
+def find_contours(picture, kernel_limit):
+    img = cv2.imread(picture)
+
+    # median blur
+    median = cv2.medianBlur(img, 5)
+
+    # threshold on black
+    lower = (0, 0, 0)
+    upper = (45, 45, 45)
+    # upper = (15, 15, 15)
+    thresh = cv2.inRange(median, lower, upper)
+
+    # apply morphology open and close
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    morph = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (kernel_limit, kernel_limit))
+    morph = cv2.morphologyEx(morph, cv2.MORPH_CLOSE, kernel)
+
+    # filter contours on area
+    contours = cv2.findContours(morph, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = contours[0] if len(contours) == 2 else contours[1]
+
+    result = img.copy()
+    line1 = []
+    line2 = []
+
+    # find ecg lines
+    for i in range(len(contours)):
+        area = cv2.contourArea(contours[i])
+        if area > 3000:
+            contour = np.ndarray.tolist(contours[i])
+            prom = []
+            for item in contour:
+                prom.append(item[0])
+
+            mi = min([item[0] for item in prom])
+            ma = max([item[0] for item in prom])
+
+            start_line = []
+            j = 0
+
+            while prom[j][0] != mi:
+                start_line.append(prom[j])
+                j += 1
+
+            start_line.append(prom[j])
+            start_line.reverse()
+            start = j
+
+            while prom[j][0] != ma:
+                j += 1
+
+            end_line = prom[j:]
+            end_line.reverse()
+
+            line1 = start_line + end_line
+            line2 = prom[start:j + 1]
+            # cv2.drawContours(result, [contours[i]], -1, (0, 0, 255), 2)
+
+    # cv2.namedWindow('Electric cardiogram of heart', cv2.WINDOW_NORMAL)
+    # cv2.imshow('Electric cardiogram of heart', result)
+
+    # if cv2.waitKey(0) & 0xFF == ord('q'):
+    #     cv2.destroyAllWindows()
+
+    return line1, line2
--- main.py
+++ main.py
@@ -1,80 +1,31 @@
 import cv2
 import numpy as np
 import math
+
+import find_contours
 import rectangle
 import curve_analysis
 
 
 def main():
-    picture = "img/without_shadow_CUT.jpg"
+    picture = "img/magic_CUT.jpg"
     # img = cv2.imread(picture, cv2.IMREAD_GRAYSCALE)
 
     img = cv2.imread(picture)
     img_height, img_width, img_channels = img.shape
-
-    # median blur
-    median = cv2.medianBlur(img, 5)
-
-    # threshold on black
-    lower = (0, 0, 0)
-    upper = (45, 45, 45)
-    # upper = (15, 15, 15)
-    thresh = cv2.inRange(median, lower, upper)
-
-    # apply morphology open and close
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-    morph = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (29, 29))
-    morph = cv2.morphologyEx(morph, cv2.MORPH_CLOSE, kernel)
-
-    # filter contours on area
-    contours = cv2.findContours(morph, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    contours = contours[0] if len(contours) == 2 else contours[1]
     result = img.copy()
-    line1 = []
-    line2 = []
 
-    # find ecg lines
-    for i in range(len(contours)):
-        area = cv2.contourArea(contours[i])
-        if area > 3000:
-            contour = np.ndarray.tolist(contours[i])
-            prom = []
-            for item in contour:
-                prom.append(item[0])
-
-            mi = min([item[0] for item in prom])
-            ma = max([item[0] for item in prom])
-
-            start_line = []
-            j = 0
-
-            while prom[j][0] != mi:
-                start_line.append(prom[j])
-                j += 1
-
-            start_line.append(prom[j])
-            start_line.reverse()
-            start = j
-
-            while prom[j][0] != ma:
-                j += 1
-
-            end_line = prom[j:]
-            end_line.reverse()
-
-            line1 = start_line + end_line
-            line2 = prom[start:j + 1]
-            # cv2.drawContours(result, [contours[i]], -1, (0, 0, 255), 2)
+    upper_contour, _ = find_contours.find_contours("img/magic_CUT.jpg", 29)
+    _, lower_contour = find_contours.find_contours("img/magic_CUT.jpg", 11)
 
     # show coordinates and scale
-    print(f'Coordinates of line: {line1}')
+    print(f'Coordinates of line: {upper_contour}')
     small_rectangle_length, axis_variants = rectangle.find_small_scale(picture)
     big_rectangle_length = 5 * small_rectangle_length
     print(f'Scale: 1 second is {big_rectangle_length} px')
 
     # find main axis
-    point_axis = max(item[1] for item in line1)
+    point_axis = max(item[1] for item in upper_contour)
     ind = 0
     while axis_variants[ind] < point_axis:
         ind += 1
@@ -84,18 +35,32 @@ def main():
     cv2.line(result, (0, real_axis), (img_width, real_axis), (255, 0, 0), 2)
 
     # show points of ecg line
-    for item in line1:
+    for item in upper_contour:
         cv2.circle(result, (item[0], item[1]), radius=1, color=(255, 0, 0), thickness=-1)
 
+    # find minimums and maximums
+    minimums, maximums = curve_analysis.find_extremes(upper_contour)
+
+    # DANGEROUS PART!!!
+    _, maximums = curve_analysis.find_extremes(upper_contour)
+    potential_minimums, _ = curve_analysis.find_extremes(lower_contour)
+    minimums = []
+
+    for i in range(1, len(maximums), 3):
+        for minimum in potential_minimums:
+            if abs(maximums[i][0] - minimum[0]) <= small_rectangle_length:
+                minimums.append(minimum)
+
     # show minimums and maximums
-    minimums, maximums = curve_analysis.find_extremes(line1)
-    minimum_letters = ['Q', 'S', '']
+    minimum_letters = ['Q', 'S']
     maximum_letters = ['P', 'R', 'T']
 
     for i in range(len(minimums)):
         x, y = minimums[i][0], minimums[i][1]
         cv2.circle(result, (x, y), radius=3, color=(0, 0, 255), thickness=-1)
-        cv2.putText(result, minimum_letters[i % 3], (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        cv2.putText(result, minimum_letters[i % 2], (x - 20, y + 20) if i % 2 == 0 else (x + 5, y + 20),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        # cv2.putText(result, minimum_letters[i % 3], (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
     for i in range(len(maximums)):
         x, y = maximums[i][0], maximums[i][1]
