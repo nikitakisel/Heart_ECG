035fde20186a31db00eb8c39eca2c3b10927460d
Никита Киселев <nikitakiselev@MacBook-Air-Nikita.local>
2024-11-01 13:53:14 +0300

    add func for layout drawing

--- curve_calculation.py
+++ curve_calculation.py
@@ -1,4 +1,52 @@
+import cv2
 import math
+
+
+def draw_lower_layout(img, x1, x2, y, layout_length, text):
+    cv2.line(img, (x1, y), (x1, y + layout_length), (0, 0, 0), 1)
+    cv2.line(img, (x2, y), (x2, y + layout_length), (0, 0, 0), 1)
+    cv2.line(img, (x1, y + layout_length), (x2, y + layout_length), (0, 120, 255), 2)
+
+    cv2.line(img, (x1, y + layout_length), (x1 + 5, y + layout_length + 3), (0, 0, 0), 2)
+    cv2.line(img, (x1, y + layout_length), (x1 + 5, y + layout_length - 3), (0, 0, 0), 2)
+    cv2.line(img, (x1 + 5, y + layout_length - 3), (x1 + 5, y + layout_length + 3), (0, 0, 0), 2)
+
+    cv2.line(img, (x2 - 5, y + layout_length - 3), (x2, y + layout_length), (0, 0, 0), 2)
+    cv2.line(img, (x2 - 5, y + layout_length + 3), (x2, y + layout_length), (0, 0, 0), 2)
+    cv2.line(img, (x2 - 5, y + layout_length - 3), (x2 - 5, y + layout_length + 3), (0, 0, 0), 2)
+
+    cv2.putText(img, text, ((x1 + x2) // 2 - 10, y + layout_length + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (255, 0, 0), 1)
+
+
+def draw_upper_layout(img, x1, x2, y, layout_length, text):
+    cv2.line(img, (x1, y), (x1, y - layout_length), (0, 0, 0), 1)
+    cv2.line(img, (x2, y), (x2, y - layout_length), (0, 0, 0), 1)
+    cv2.line(img, (x1, y - layout_length), (x2, y - layout_length), (0, 120, 255), 2)
+
+    cv2.line(img, (x1, y - layout_length), (x1 + 5, y - layout_length + 3), (0, 0, 0), 2)
+    cv2.line(img, (x1, y - layout_length), (x1 + 5, y - layout_length - 3), (0, 0, 0), 2)
+    cv2.line(img, (x1 + 5, y - layout_length - 3), (x1 + 5, y - layout_length + 3), (0, 0, 0), 2)
+
+    cv2.line(img, (x2 - 5, y - layout_length - 3), (x2, y - layout_length), (0, 0, 0), 2)
+    cv2.line(img, (x2 - 5, y - layout_length + 3), (x2, y - layout_length), (0, 0, 0), 2)
+    cv2.line(img, (x2 - 5, y - layout_length - 3), (x2 - 5, y - layout_length + 3), (0, 0, 0), 2)
+
+    cv2.putText(img, text, ((x1 + x2) // 2 - 10, y - layout_length - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (255, 0, 0), 1)
+
+
+def draw_layout(img, maximums, establish_points, small_rectangle_length):
+    for i in range(len(establish_points) // 6):
+        draw_lower_layout(img, establish_points[6 * i][0],  establish_points[6 * i + 2][0],
+                          establish_points[6 * i][1], small_rectangle_length * 3, "Int PQ")
+        draw_lower_layout(img, establish_points[6 * i + 2][0], establish_points[6 * i + 3][0],
+                          establish_points[6 * i + 2][1], small_rectangle_length * 5, "QRS")
+        draw_lower_layout(img, establish_points[6 * i + 2][0], establish_points[6 * i + 5][0],
+                          establish_points[6 * i + 2][1], small_rectangle_length * 8, "Int QT")
+
+        draw_upper_layout(img, establish_points[6 * i + 1][0], establish_points[6 * i + 2][0],
+                          establish_points[6 * i + 1][1], small_rectangle_length * 3, "Seg PQ")
+        draw_upper_layout(img, establish_points[6 * i + 3][0], establish_points[6 * i + 4][0],
+                          establish_points[6 * i + 3][1], small_rectangle_length * 3, "Seg ST")
 
 
 def print_parameters(maximums, establish_points, scale):
--- main.py
+++ main.py
@@ -48,13 +48,17 @@ def main():
     potential_minimums, _ = curve_analysis.find_extremes(lower_contour)
     minimums = curve_analysis.find_neighbour_minimums(maximums, 3, [1], potential_minimums)
 
-    # find and draw establish points
+    # find establish points
     potential_establish_points = curve_analysis.find_establish_points(upper_contour, small_rectangle_length)
     establish_points = \
         curve_analysis.find_neighbour_minimums(maximums, 3, [0, 2], potential_establish_points) + \
         curve_analysis.find_neighbour_maximums(minimums, 5, potential_establish_points)
     establish_points.sort()
 
+    # show a layout
+    curve_calculation.draw_layout(result, maximums, establish_points, small_rectangle_length)
+
+    # draw establish points
     for item in establish_points:
         cv2.circle(result, (item[0], item[1]), radius=3, color=(0, 120, 255), thickness=-1)
 
@@ -66,13 +70,13 @@ def main():
         x, y = minimums[i][0], minimums[i][1]
         cv2.circle(result, (x, y), radius=3, color=(0, 0, 255), thickness=-1)
         cv2.putText(result, minimum_letters[i % 2], (x - 20, y + 20) if i % 2 == 0 else (x + 5, y + 20),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
         # cv2.putText(result, minimum_letters[i % 3], (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
     for i in range(len(maximums)):
         x, y = maximums[i][0], maximums[i][1]
         cv2.circle(result, (x, y), radius=3, color=(44, 211, 15), thickness=-1)
-        cv2.putText(result, maximum_letters[i % 3], (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (44, 211, 15), 2)
+        cv2.putText(result, maximum_letters[i % 3], (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (44, 211, 15), 2)
 
     # print parameters calculation
     curve_calculation.print_parameters(maximums, establish_points, real_scale)
